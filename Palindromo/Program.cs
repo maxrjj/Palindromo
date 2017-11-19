f24e3cf0a61e954d117780ff2a881fedf9a92739
Marcio Vasconcelos <maxrjj@hotmail.com>
2017-11-19 12:59:47 -0200

    Commit solução

---
+++ Palindromo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array =
+        {
+            "Dot",
+            "Net",
+            "Perls",
+            "Is",
+            "Not",
+            "A",
+            "Palindrome",
+            ""
+        };
+
+            foreach (string value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+            }
+        }
+
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+    }
+}
